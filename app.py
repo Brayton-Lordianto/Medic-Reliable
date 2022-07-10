bebfb260e48da8d52e64d13589ef8c7758791fa0
Brayton-Lordianto <braylord30@gmail.com>
2022-07-10 18:13:03 +0800

    Frontend for register

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from urllib import request
-# open terminal. source .venv/bin/activate
+# open terminal. source .venv/bin/activate and run there
 
 from flask import Flask, render_template, request
-import pymongo
 
+# instantiate the app
+app = Flask(__name__)
 
+# route for home
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# run programmatically
+if __name__ == "__main__":
+    app.run(debug = True)
+    
